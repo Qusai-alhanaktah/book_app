c4f1033a73fade8c40f4bf63fed6837da3325a23
Qusai-alhanaktah <qusaialhanaktah@gmail.com>
2020-01-04 18:53:30 +0200

    still working with add to data base

--- server.js
+++ server.js
@@ -1,24 +1,62 @@
-'sue strict';
+'use strict';
+require('dotenv').config();
 const express = require('express');
 const server = express();
 const cors = require('cors');
 
-
 const superagent = require('superagent');
-require('dotenv').config();
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL);
 
 const PORT = process.env.PORT || 3000;
 
 server.set('view engine', 'ejs')
-server.use(express.static('public'));
+server.use(express.static('./public'));
 server.use(express.urlencoded({ extended: true }));
 
-server.get('/', (req, res) => {
-    res.render('pages/index')
-});
+function handleError(error, response){
+    response.render('pages/error', {error: error});
+}
 
 
-server.post('/searches', (req, res) => {
+server.get('/', getAllBooks);
+server.get('/new', addBooks);
+server.post('/search', searcheData);
+server.post('/new', processAdd);
+server.get('/books/:book_id', getSpecificBook);
+
+function getSpecificBook(req,res){
+    let SQL = `SELECT * FROM book WHERE id=$1`;
+    let id = req.params.book_id;
+    let values =[id];
+    client.query(SQL,values)
+    .then(data=>{
+        res.render('pages/books/detail.ejs',{book: data.rows});
+    }).catch(err=>handleError(err));
+}
+
+function getAllBooks(req,res){
+    let SQL = `SELECT * FROM book;`;
+    client.query(SQL)
+    .then(data=>{
+        res.render('pages/index',{booksList: data.rows});
+    }).catch(err=>hanleError(err));
+}
+function addBooks(req,res){
+    res.render('pages/searches/new')
+}
+function processAdd(req,res){
+    console.log(req.body)
+    let { image_url, title, author, description, isbn, bookshelf } = req.body
+    let SQL = `INSERT INTO books (image_url, title, author, description, isbn, bookshelf) VALUES ($1, $2, $3, $4, $5, $6)`
+    let values = [image_url, title, author, description, isbn, bookshelf]
+
+    client.query(SQL, values)
+    .then( () => {
+        res.redirect('/');
+    }).catch(err => handleError(err));
+}
+function searcheData(req, res){
     const url = `https://www.googleapis.com/books/v1/volumes?q=${req.body.select}+${req.body.input}`;
     superagent.get(url)
         .then(data => {
@@ -26,7 +64,7 @@ server.post('/searches', (req, res) => {
             let book = jsaonData.map(data => new Book(data));
             res.render('pages/searches/show', {books:book});
         })
-})
+}
 function Book(data) {
     this.title = data.volumeInfo.title? data.volumeInfo.title: "No Title Available";
     this.imgUrl = (data.volumeInfo.imageLinks && data.volumeInfo.imageLinks.thumbnail) ? data.volumeInfo.imageLinks.thumbnail:"https://i.imgur.com/J5LVHEL.jpg";
@@ -41,5 +79,7 @@ server.get('*', (req, res) => {
     res.status(404).render('./pages/error', { 'message': message })
 });
 
-
-server.listen(PORT, () => console.log(PORT, 'YAAAAAAA'));
+client.connect()
+.then(()=>{
+    server.listen(PORT, () => console.log(PORT, 'YAAAAAAA'));
+}).catch(err => handleError(err));
